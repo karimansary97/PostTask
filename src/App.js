f51f9282720f1ec7ec9eab641b5331629c464291
kareem Ansary <kareem.ansary@gmail.com>
2022-10-04 02:31:16 +0200

    Create-Home-Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import UserCard from "./Components/UserCard";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <UserCard />
+      <Home />
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UserCard from "../Components/UserCard";
+
+function Home() {
+    const array={}
+  return (
+    <div>
+      <UserCard />
+    </div>
+  );
+}
+
+export default Home;
